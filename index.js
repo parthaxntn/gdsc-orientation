f731e53f25e4b8c02698452bdb33fb64db18481b
Partha_Pratim_Deka <Parthapratimdeka129@gmail.com>
2023-01-07 13:23:50 +0530

    initial-commit

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require('express')
+const { join } = require('path')
+const path = require('path')
+const bodyParser = require('body-parser')
+const pug = require('pug')
+const datafile = require('./public/data.json')
+const fs = require('fs')
+
+const app = express()
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+app.set('view engine', 'pug')
+app.use(express.static('public'))
+
+app.get('/gdsc/bla-bla', (req, res) => {
+  res.render(path.join(__dirname+'/pages/index'))
+})
+
+app.post('/wlcm', urlencodedParser, (req, res) => {
+    const { name } = req.body
+    if (!name) {
+        res.render(path.join(__dirname+'/pages/index'), { noname: true })
+    }
+    const data = {
+        name: name,
+        time: new Date().toLocaleString()
+    }
+    datafile.data.push(data)
+    fs.writeFileSync('./public/data.json', JSON.stringify(datafile))
+    res.render(path.join(__dirname+'/pages/wlcm'), { name, noname: false })
+})
+
+app.get('/read',(req,res)=>{
+    res.json(datafile)
+})
+
+app.listen(5000, () => {
+    console.log('Example app listening on port 3000!')
+})
